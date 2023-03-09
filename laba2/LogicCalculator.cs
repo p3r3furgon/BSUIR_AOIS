df712fb985bc5ba3acaeed66b85cba19c487ae09
p3r3furgon <zayats.dz@gmail.com>
2023-03-09 10:05:16 +0300

    LR2 is ready for demonstration

--- laba2/LogicCalculator.cs
+++ laba2/LogicCalculator.cs
@@ -84,4 +84,27 @@ namespace AOIS_2
             return x1 == x2;
         }
     }
+
+    internal static class Priority
+    {
+        private static readonly Dictionary<string, int> priorities;
+
+        static Priority()
+        {
+            priorities = new Dictionary<string, int>();
+            priorities.Add("!", 5);
+            priorities.Add("*", 4);
+            priorities.Add("+", 3);
+            priorities.Add("->", 2);
+            priorities.Add("==", 1);
+        }
+
+        public static int GetPriority(string sign)
+        {
+            if (priorities.ContainsKey(sign))
+                return priorities[sign];
+            else
+                return 0;
+        }
+    }
 }
